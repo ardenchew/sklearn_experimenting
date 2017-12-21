2c01940d0c48156af400c55121c661c23285e5e3
Arden Chew <arden@thechewfamily.com>
2017-12-20 20:57:24 -0500

    Add files via upload

---
+++ iris_classification.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from sklearn import datasets
+
+iris = datasets.load_iris()
+digits = datasets.load_digits()
+
